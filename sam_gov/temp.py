e3d4ec089613eeb10721884b71df4ac0b46f0548
nour662 <nour@mtechlt040.umd.edu>
2024-10-02 14:30:26 -0400

    temp.py

--- sam_gov/temp.py
+++ sam_gov/temp.py
@@ -9,25 +9,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotInteractableException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotInteractableException , InvalidSelectorException
 
 
 
 
-def search_keyword(keyword):
- 
-    pass 
+def search_keyword(driver, keyword):   
 
+    search_bar = driver.find_element(By.XPATH , '//input[@name = "search"]' )
+    submit_button = driver.find_element(By.XPATH , '//button[@class="usa-button ng-star-inserted"]')
 
-def get_links(): 
-    pass 
+    search_bar.send_keys(keyword)
+    submit_button.click() 
 
+    sleep(10)
+    links  = driver.find_elements(By.XPATH , '//div[@class="grid-row grid-gap"]//a')
+
+    return [a.get_attribute('href') for a in links]
 
 
 def scrape_links(): 
     pass 
-
 
 
 def main():
@@ -38,20 +44,34 @@ def main():
     chrome_options = Options()
     chrome_options.add_argument('--remote-debugging-port=9222')  # Debugging port; remove if not needed
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    
+
+    driver.get("https://sam.gov/content/home")
+
+    sleep(60)
+
+    search_page_button = driver.find_element(By.XPATH, '//a[@id="search"]')
+    search_page_button.click() 
 
     input_df = pd.read_csv("name_list.csv")
     input_list =  input_df["Company_Name"].tolist() 
 
-    
+
+
+    domain_button = driver.find_element(By.XPATH , '//div[@class="sds-card sds-card--collapsible sds-card--collapsed ng-star-inserted"]')
+    domain_button.click()
+
+
+    entity_domain = driver.find_element(By.XPATH , '(//li[@class="usa-sidenav__item ng-star-inserted"])[3]')
+    entity_domain.click()
+
 
     for name in input_list: 
         search_input = name.lower().strip() 
 
         search_input = search_input.replace("." , "").replace("," , "").replace("inc" , "").replace("llc" , "").replace("corp " , "").replace("ltd" , "").replace("limited" , "").replace("pty" , "")
         
-        search_keyword(search_input) 
-
-        result_links = get_links() 
+        result_links = search_keyword(driver, search_input) 
 
         links[name] = result_links
 
