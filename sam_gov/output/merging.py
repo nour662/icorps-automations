1cc99e41b47917852c8e880f2aa00d36d87b183e
Scott Lucarelli <scottluc@terpmail.umd.edu>
2024-10-14 16:04:06 -0400

    Cleaned batches 24-45

--- sam_gov/output/merging.py
+++ sam_gov/output/merging.py
@@ -6,6 +6,26 @@ all_files = glob.glob(r'C:\Users\sluca\Downloads\icorps-data\sam_gov\output\batc
 df_list = []
 
 for file in all_files:
+    # Extract the batch number from the file name
+    batch_number = int(file.split('batch_')[1].split('.csv')[0])
+    
+    # Check if the batch number is within the desired range
+    if 24 <= batch_number <= 45:
+        try:
+            temp_df = pd.read_csv(file)
+            df_list.append(temp_df)
+        except Exception as e:
+            print(f"Error reading {file}: {e}")
+
+if df_list:
+    merged_df = pd.concat(df_list, ignore_index=True)
+    merged_df.to_csv('merged_batches(24-45).csv', index= False)
+
+"""
+
+df_list = []
+
+for file in all_files:
     try:
         temp_df = pd.read_csv(file)
         df_list.append(temp_df)
@@ -13,4 +33,5 @@ for file in all_files:
         print(f"Error reading {file}: {e}")
 
 merged_df = pd.concat(df_list, ignore_index= True)
-merged_df.to_csv('merged_batches(1-23).csv', index=False)
+merged_df.to_csv('merged_batches(23-45).csv', index=False)
+"""
