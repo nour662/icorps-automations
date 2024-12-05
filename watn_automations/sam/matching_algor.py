4c74b980887fb0be2a4fe47e285a012140c84782
nour662 <nourali1786@gmail.com>
2024-12-05 14:32:04 -0500

    omfg i almost lost these files

--- watn_automations/sam/matching_algor.py
+++ watn_automations/sam/matching_algor.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-def clean_masterlist(df, output_file): 
+def clean_masterlist(df): 
     df["Full Name"] = df["First Name"] + " " + df["Last Name"]
     
     df = df.drop(columns=["First Name", "Last Name"])
@@ -11,35 +11,54 @@ def clean_masterlist(df, output_file):
             "Shipping Street", 
             "Shipping City", 
             "Shipping State/Province", 
-            "Shipping Country"]]
+            "Shipping Country", 
+            "Website"]]
         .agg(lambda x: list(x))  
         .reset_index()
     )
     
-    # Join the 'Full Name' column into a comma-separated list
-    grouped['sf_full_names'] = grouped['Full Name'].apply(lambda x: ", ".join(x))
-    # For all other columns, take the first value (assuming they are the same across rows per company)
+    grouped['sf_company_name'] = grouped['Company'].lower()
+    grouped['sf_contacts'] =grouped['Full Name'].apply(lambda x : x.lower())
     grouped['sf_street'] = grouped['Shipping Street'].apply(lambda x: x[0])
     grouped['sf_city'] = grouped['Shipping City'].apply(lambda x: x[0])
     grouped['sf_state'] = grouped['Shipping State/Province'].apply(lambda x: x[0])
     grouped['sf_country'] = grouped['Shipping Country'].apply(lambda x: x[0])
+    grouped['sf_website'] = grouped['Website'].apply(lambda x: x[0])
 
-
-    grouped = grouped.drop(columns=[ "Full Name", "Shipping Street", "Shipping City" , "Shipping State/Province", "Shipping Country" ])
-    print(grouped)
-
-    grouped.to_csv(output_file, index=False)
-    print(f"Data saved to {output_file}")
-
+    grouped = grouped.drop(columns=[ "Company", "Full Name", "Shipping Street", "Shipping City" , "Shipping State/Province", "Shipping Country" , "Website" ])
+    
     return grouped
+
+def clean_training(df): 
+
+   df["sam_company_name"] =  df["legal_name"].lower()
+    
+
+
+
+def fuzzy_match(master_list, ): 
+    pass
+
+
+def train_model(supervised): 
+
+
+    pass 
 
 
 def main(): 
-    input_file = "../inputs/icorps_masterlist.csv"
-    output_file = "cleaned_masterlist.csv"
+    input_file_one = "../inputs/icorps_masterlist.csv"
+    input_file_two = "../outputs/outputs_11212024/merged_files/same_uncleaned.csv" 
 
-    master_list = pd.read_csv(input_file)
-    clean_masterlist(master_list, output_file)
+    training_csv = "training.csv"
+
+
+    master_df = pd.read_csv(input_file_one)
+    master_df  = clean_masterlist(master_df)
+
+    training_df = pd.read_csv(training_csv)
+    clean_training(training_df)
+
     
 
 if __name__ == "__main__": 
