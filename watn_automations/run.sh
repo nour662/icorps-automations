d49107a3f86f3e6497e2c9d32f88a0d6e86eeb84
nour662 <nourali1786@gmail.com>
2025-03-09 16:49:11 -0400

    finished pitchbook scraper

--- watn_automations/run.sh
+++ watn_automations/run.sh
@@ -1,24 +1,29 @@
-source ../util/myenv/bin/activate
+## 1. Starting Virtual Requirements
+source util/myenv/bin/activate
+
+## 2. Installing Dependencies
+pip install -r util/requirements.txt --break-system-packages
+
+## 3. Identify Date
 today=$(date +"%m%d%Y")
 
-rm -R outputs/outputs_$today
-
-
-
+## 4. Creating Output Directory
 mkdir outputs/outputs_$today
 
-cd outputs/outputs_$today
+## 5. Creating Sepereate Directories for Data Sources
+cd outputs/outputs_$today && mkdir usas_batches sam_batches sbir_batches merged_batches && cd ../..
 
-mkdir usas_batches sam_batches sbir_batches log merged_files 
+## 6. Getting cookies.pkl File for SAM.gov login
+python3 util/get_cookies.py -i https://sam.gov  -o sam/cookies.pkl 
 
-cd ../..
-
-#python3 get_cookies.py -i https://sam.gov  -o ../watn_automations/sam/cookies.pkl
-
+## 7. Running SAM.gov Scraper
 python3 sam/sam_scraper.py -i inputs/icorps_masterlist.csv -s 0 -o outputs/outputs_$today
 
 #python3 ../util/merge_batches.py -r outputs/outputs_$today -s sam
 
+mkdir 
+
+python3 matching_algor.py  
 
 ## Matching algorithm for sam 
 
