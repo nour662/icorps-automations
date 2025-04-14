9f370a6ada9c546971093ec54fb47cf46e1ca676
nour662 <nourali1786@gmail.com>
2025-04-14 16:09:54 -0400

    pushing usas

--- watn_automations/usas/api_test.py
+++ watn_automations/usas/api_test.py
@@ -1,10 +1,13 @@
 import requests
 import json
 import pandas as pd
+import numpy as np
+
+
 
 url = "https://api.usaspending.gov/api/v2/search/spending_by_award/"
 
-ueis = pd.read_csv('test.csv')['num_uei'].tolist() 
+ueis = pd.read_csv('test.csv')['UEI'].tolist() 
 
 headers = {
     "Content-Type": "application/json"
@@ -19,30 +22,36 @@ payload_template = {
         "Recipient Name", "Start Date", "End Date", "Award Amount",
         "Awarding Agency", "Awarding Sub Agency", "Contract Award Type",
         "Award Type", "Funding Agency", "Funding Sub Agency"
-    ]}
+    ], 
+    
+    "limit": 100}
 
 all_data = []
 
 for uei in ueis:
-    payload = payload_template.copy()
-    payload["filters"]["recipient_search_text"] = [uei]
+    if not uei is np.nan : 
+        payload = payload_template.copy()
+        payload["filters"]["recipient_search_text"] = [uei]
 
-    response = requests.post(url, headers=headers, json=payload)
+        response = requests.post(url, headers=headers, json=payload)
 
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+            awards = data.get("results", [])
+            
+            if awards:
+                for award in awards:
+                    all_data.append(award)
+        else:
+            print(f"Request failed for UEI {uei} with status code {response.status_code}")
+    else: 
+        pass 
 
-        awards = data.get("results", [])
-        
-        if awards:
-            for award in awards:
-                all_data.append(award)
-    else:
-        print(f"Request failed for UEI {uei} with status code {response.status_code}")
+
 
 df = pd.DataFrame(all_data)
 
-# Save the DataFrame to a CSV file
+
 df.to_csv('spending_data.csv', index=False)
 print("Data has been saved to 'spending_data.csv'.")
