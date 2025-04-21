95137e2d38674ecc74e79b7052c492c09c4ae65d
GGross27 <gaby.ama.gross@gmail.com>
2025-04-21 15:59:39 -0400

    Working usas scrapper for contract/grant data extraction and general company data extraction

--- watn_automations/usas/usas_api_scraper.py
+++ watn_automations/usas/usas_api_scraper.py
@@ -1,88 +1,9 @@
 import pandas as pd
-from time import sleep
-from lxml import html
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import os
 import numpy as np
 import csv
 import requests
 import json
 
-# graveyard >:3
-'''
-input_file = "watn_automations\\usas\\small_uei.csv"
-    if not os.path.exists(input_file):
-        print(f"Input file '{input_file}' not found.")
-        return
-
-    input_df = pd.read_csv(input_file)
-    if 'num_uei' not in input_df.columns:
-        print("The CSV must contain a 'UEI' column.")
-        return
-
-    input_list = input_df['num_uei'].tolist()
-
-    # # Configure Chrome WebDriver options
-    # chrome_options = Options()
-    # chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--remote-debugging-port=9230")
-    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-    
-    # Award Data extracted for Grants -- not available for every UEI  
-    payload2=  {"filters": {
-        "recipient_search_text":['JD35KEUVYS94'],
-        "award_type_codes": ["02", "03" , "04" , "05"]
-      },
-      "fields": ["Recipient Name", "Start Date",
-          "End Date",
-          "Award Amount",
-          "Awarding Agency",
-          "Awarding Sub Agency",
-          "Contract Award Type",
-          "Award Type",
-          "Funding Agency",
-          "Funding Sub Agency"],
-      "limit": 100
-    }
-
-    response1 = requests.post(base_url, headers=header, json=payload1)
-    response2 = requests.post(base_url, headers=header, json=payload2)
-    # print(response.status_code)
-    if response1.status_code == 200 and response2.status_code == 200:
-        data1 = response1.json()
-        print("data2:", data1)
-        data2 = response2.json()
-        print("data2:", data2)
-
-   
-    # need to read UEI's from input csv
-    def uei_list (file_path):
-        with open (file_path, mode = 'r') as file:
-            reader = csv.DictReader(file)
-            return [row["UEI"].strip() for row in reader if row["UEI"].strip()] 
-            # list is going to be unfriendly, write the loop and append "carter will do this"
-
-    #search recipient and award data
-    def get_data (uei):
-        response = requests.post( # find uei
-            base_url,
-            json = {"text":uei} #put uei into csv, with column uei
-        )
-        
-    # response.raise_for_status
-    # data = response.json()
-    # if not data["results"] :
-    #     return None
-    # return data["results"][0]
-'''
 def get_company_info(recipient_ids, data_list): 
     for recipient_id in recipient_ids:
         recipient_url = f"https://api.usaspending.gov/api/v2/recipient/{recipient_id}/"
@@ -182,6 +103,7 @@ def main():
                 response1 = requests.post(base_url, headers=header, json=payload)
                 if get_data(response1, all_contract_data) :
                     all_contract_data = get_data(response1, all_contract_data)
+                    print(f'{uei} Contract Data Processed')
                 else:
                     print(f"Request failed for UEI {uei} with status code {response1.status_code}")
                 
@@ -189,10 +111,10 @@ def main():
                 payload["filters"]["award_type_codes"] = ["02", "03" , "04" , "05"]
                 response2 = requests.post(base_url, headers=header, json=payload)
                 if get_data(response2, all_grant_data):
-                    all_data_grant = get_data(response2, all_grant_data)
+                    all_grant_data = get_data(response2, all_grant_data)
+                    print(f'{uei} Grant Data Processed')
                 else: 
-                    print(f"Request failed for UEI {uei} with status code {response2.status_code}")
-                print(f'{uei} Processed')
+                    print(f"Request failed for UEI {uei} with status code {response2.status_code}")        
             else: 
                 pass 
     except Exception as e:
